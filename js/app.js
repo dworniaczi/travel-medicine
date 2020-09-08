9e6e032d650c2c95db06880bdb7ed9e1d37ca8a3
Tomek <tomasz.dworniczak@hotmail.com>
2020-09-08 17:22:08 +0200

    Add slide toggle function to the certificate section, add scroll into view function to certificate link and prices button

--- js/app.js
+++ js/app.js
@@ -3,3 +3,28 @@ $(document).ready(function(){
         $(".container-table").slideToggle(800);
     });
 });
+
+$(document).ready(function(){
+    $(".certificate-link").click(function(){
+        $(".container-certificate").slideToggle(800);
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('.button-prices').click(function(){
+        $('html,body').animate({
+            scrollTop: $('.button-prices').offset().top},
+            'slow'
+        );
+    });
+});
