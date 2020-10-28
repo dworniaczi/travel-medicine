07c2a07300d0010ef2712c04e336b8ea86af7e55
Tomek <tomasz.dworniczak@hotmail.com>
2020-10-28 12:05:04 +0100

    Delete unused styles and functions, change style in menu button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-    $(".button-prices").click(function(){
-        $(".container-table").slideToggle(800);
-    });
-});
-
-$(document).ready(function(){
     $(".certificate-link").click(function(){
         $(".container-certificate").slideToggle(800);
     });
@@ -17,15 +11,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
-    });
-});
-
-$(document).ready(function(){
-    $('.button-prices').click(function(){
-        $('html,body').animate({
-            scrollTop: $('.button-prices').offset().top},
-            'slow'
-        );
     });
 });
 
